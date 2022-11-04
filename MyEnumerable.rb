2ec840349ada3fc35ce58f2a4f5300233ce16c71
skyv26 <dev.aakashv@gmail.com>
2022-11-04 19:46:42 +0530

    Update the `.rb` file

---
+++ MyEnumerable.rb
@@ -0,0 +1,10 @@
+# Create own Enumerable
+
+module MyEnumerable
+
+  def all?
+    # return true when none of the collection members are false or ni.
+    
+  end
+
+end
--- MyList.rb
+++ MyList.rb
@@ -1,11 +1,13 @@
 # Create MyList Class that has an instance variable @list.
 
-class MyList
+require_relative 'MyEnumerable'
 
+class MyList
+  include MyEnumerable
   def initialize(*args)
     @list = args
   end
-
+ 
   def each
     yield @list
   end
@@ -13,3 +15,5 @@ end
 
 myObj = MyList.new(1, 2, 3, 4)
 #<MyList:0x00005582d47bbce8 @list=[1, 2, 3, 4]
+
+myObj.all? { |e| e == 2 }
