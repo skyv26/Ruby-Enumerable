96fda903f6ac74dd3a8bba1b83123a8457ae68c0
kusi <lawcubegsm@gmail.com>
2022-11-05 18:55:36 +0000

    Fix error

--- my_list.rb
+++ my_list.rb
@@ -10,10 +10,3 @@ class MyList
     @list.each { |value| yield value if block_given? }
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-list.all? { |e| e < 5 } # => true
-list.all? { |e| e > 5 } # => false
-list.any? { |e| e == 2 } # => true
-list.any? { |e| e == 5 } # => false
-list.filter &:even? # > [2,4]
